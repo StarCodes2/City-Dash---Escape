5c5de0ba7cb0612c94a8b341b369d46f3cdcbbfb
miraculous04 <miracleaderinkola@gmail.com>
2022-12-03 12:15:52 +0100

    Spawn Error
    spawn Error Corrected

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -104,14 +104,6 @@ public class PlayerMotor : MonoBehaviour
         {
             targetPosition += Vector3.right * laneDisance;
         }
-
-        // Player to Jump
-        if(Input.GetKeyDown(KeyCode.Space) && onGround)
-        {
-            PlayerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            onGround = false;
-            StartJumping();
-        }
         moveVector.x = (targetPosition - transform.position).x * speed;
 
         //transform.position = targetPosition;
@@ -123,12 +115,6 @@ public class PlayerMotor : MonoBehaviour
         controller.Move(moveVector * Time.fixedDeltaTime);
     }
 
-    public void StartJumping()
-    {
-        Jumping = true;
-        anim.SetTrigger("StartJumping");
-        transform.Translate(0, 50, 0);
-    }
     public void StartRunning()
     {
         isRunning = true;
